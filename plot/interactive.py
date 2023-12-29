15fe50895a9b0c25829ed0526f0dd3c46fd16741
claraabmoura <152560314+claraabmoura@users.noreply.github.com>
2023-12-29 01:13:11 +0000

    fe

--- plot/interactive.py
+++ plot/interactive.py
@@ -1,5 +1,5 @@
 import plotly.express as px
-from Data.download import download_data
+from data.download import download_data
 
 def plot_line_i(ticker:str):
     """ Plot an interactive plot using plotly.
@@ -13,9 +13,14 @@ def plot_line_i(ticker:str):
 
     data = download_data(ticker)
 
+    data["SMA"] = data['Close'].rolling(window=9).mean()
+    data["LMA"] = data['Close'].rolling(window=72).mean()
+
+
     fig = px.line(
         data.reset_index(),
-        x = 'Date', y = ['Close', 'SMA', 'LMA'], title = ticker,
+        x = 'Date', y = ['Close', 'SMA', 'LMA'], 
+        title = ticker,
         labels = {'Close': 'Fechamento', 'Date': 'Data'},
         color_discrete_map={'Close': 'black', 'SMA': 'blue', 'LMA': 'red'}
     )
